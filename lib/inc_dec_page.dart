472c2f0638c87c6e4c05a59a5877d805600f2b99
Lucifer <singhanurudh570@gmail.com>
2024-01-31 13:54:34 +0530

    init commit

---
+++ lib/inc_dec_page.dart
@@ -0,0 +1,39 @@
+import 'package:counter_cubit/bloc/counter_bloc.dart';
+import 'package:counter_cubit/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class IncDecPage extends StatelessWidget {
+  const IncDecPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final counterBloc = BlocProvider.of<CounterBloc>(context);
+
+    final counterCubit = BlocProvider.of<CounterCubit>(context);
+
+    return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              counterCubit.increment();
+              // counterBloc.add(CounterIncremented());
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              counterCubit.decrement();
+              // counterBloc.add(CounterDecremented());
+            },
+            tooltip: 'Decrement',
+            child: const Icon(Icons.minimize),
+          ),
+        ],
+      ),
+    );
+  }
+}
